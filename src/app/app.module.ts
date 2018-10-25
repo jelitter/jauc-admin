6065bff1a10f9aa1407ffb9e2d6a4a6d07fa12e4
Manuel <15770711+Narshe1412@users.noreply.github.com>
2018-10-25 14:44:57 +0100

    Fixed CI configuration for Angular App (#25)
    * Updated circleCI configuration file with docker image and calling linter, tests and build
    
    * added single run parameter to testing suite
    
    * added proper testing commands from Angular.io docs
    
    * Update src/app/app.module.ts
    
    Co-Authored-By: Narshe1412 <manuel1412@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-// Angular MAterial
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule, MatSortModule } from '@angular/material';
+// Angular Material
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule, MatSortModule } from "@angular/material";
 
 // Firebase
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { environment } from '../environments/environment';
-import { CarsComponent } from './components/cars/cars.component';
-import { CarComponent } from './components/cars/car/car.component';
-import { CarListComponent } from './components/cars/car-list/car-list.component';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { environment } from "../environments/environment";
+import { CarsComponent } from "./components/cars/cars.component";
+import { CarComponent } from "./components/cars/car/car.component";
+import { CarListComponent } from "./components/cars/car-list/car-list.component";
 
 // Services
-import { CarService } from './services/car.service';
+import { CarService } from "./services/car.service";
 
 @NgModule({
   declarations: [AppComponent, CarsComponent, CarComponent, CarListComponent],
