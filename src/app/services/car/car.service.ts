82545bf9fbc0a284de739e9b19ac1dacbdf9335b
Manuel Colorado <manuel1412@gmail.com>
2018-10-31 15:35:02 +0000

    Fixed module being called and added casting to the return value of getCars()

--- src/app/services/car/car.service.ts
+++ src/app/services/car/car.service.ts
@@ -1,42 +1,41 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Car } from '../../models/car';
 import { Location } from '../../models/location';
-// import * as shared from '../../shared/js/shared';
 import { randomCorkCoords } from '../../shared/js/shared';
 
 @Injectable()
 export class CarService {
-  carList: AngularFireList<any>;
-  selectedCar: Car = new Car();
+    carList: AngularFireList<any>;
+    selectedCar: Car = new Car();
 
-  constructor(private firebase: AngularFireDatabase) {}
+    constructor(private firebase: AngularFireDatabase) {}
 
-  getCars() {
-    return (this.carList = this.firebase.list('cars'));
-  }
+    getCars(): AngularFireList<Car> {
+        return (this.carList = this.firebase.list('cars'));
+    }
 
-  addCar(car: Car) {
-    const { lat, lon } = randomCorkCoords();
+    addCar(car: Car) {
+        const { lat, lon } = randomCorkCoords();
 
-    this.carList.push({
-      name: car.name,
-      plate: car.plate,
-      location: new Location(lat, lon)
-    });
-  }
+        this.carList.push({
+            name: car.name,
+            plate: car.plate,
+            location: new Location(lat, lon)
+        });
+    }
 
-  updateCar(car: Car) {
-    const { lat, lon } = randomCorkCoords();
+    updateCar(car: Car) {
+        const { lat, lon } = randomCorkCoords();
 
-    this.carList.update(car.$key, {
-      name: car.name,
-      plate: car.plate,
-      location: new Location(lat, lon)
-    });
-  }
+        this.carList.update(car.$key, {
+            name: car.name,
+            plate: car.plate,
+            location: new Location(lat, lon)
+        });
+    }
 
-  deleteCar($key: string) {
-    this.carList.remove($key);
-  }
+    deleteCar($key: string) {
+        this.carList.remove($key);
+    }
 }
