50bcbf57d4d68e30f8aa925435c90beb5f557640
Manuel Colorado <manuel1412@gmail.com>
2018-10-31 13:18:41 +0000

    updated to latest version of angular firebase

--- src/app/services/booking/booking.service.ts
+++ src/app/services/booking/booking.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Car } from '../../models/car';
 import * as shared from '../../shared/js/shared';
 import { CarService } from '../car/car.service';
 
-
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BookingService {
-
-  constructor() { }
+    constructor() {}
 }
