b70760e0d8fb6bdce315ec08fe9ff1936ac404d4
Dominic M Brause <dom.brause@gmail.com>
2019-01-07 22:55:46 +0000

    24 booking count

--- src/app/reports/invoice/bookings-last24/bookings-last24.component.ts
+++ src/app/reports/invoice/bookings-last24/bookings-last24.component.ts
@@ -1,4 +1,7 @@
+import { Booking } from 'src/app/models/booking';
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from 'src/app/services/booking.service';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-bookings-last24',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingsLast24Component implements OnInit {
 
-  constructor() { }
+  	bookingList: Array<Booking>;
+		total: number = 0;
 
-  ngOnInit() {
-  }
+    constructor(private bookingService: BookingService) {}
+
+    ngOnInit() {
+        this.bookingService
+            .getBookings()
+            .snapshotChanges()
+            .subscribe(
+							update => {
+                this.bookingList = this.getBooking(update);
+								this.calculateTotal(this.bookingList);
+            });
+    }
+
+    getBooking(fireBaseData) {
+        const data = [];
+
+        fireBaseData.forEach(element => {
+            const booking = element.payload.toJSON();
+            booking['$key'] = element.key;
+            data.push(booking as Booking);
+        });
+
+        return data;
+    }
+
+		calculateTotal(list: Array<Booking>): void {
+			let count = 0;
+
+			list.forEach((element) => {
+				const oneday = 60 * 60 * 24 * 1000; // milliseconds
+				const dateDiff = Math.abs(new Date().getTime() - new Date(element.date).getTime());
+
+				if(dateDiff < oneday) { // if date is within 24h
+					count++;
+				}
+			});
+
+			this.total = count;
+		}
 
 }
--- src/app/reports/invoice/total-revenue/total-revenue.component.ts
+++ src/app/reports/invoice/total-revenue/total-revenue.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from 'src/app/services/invoice.service';
+import { Invoice } from 'src/app/models/invoice';
 
 @Component({
   selector: 'app-total-revenue',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TotalRevenueComponent implements OnInit {
 
-  constructor() { }
+    invoiceList: Array<Invoice>;
 
-  ngOnInit() {
-  }
+    constructor(private invoiceService: InvoiceService) {}
+
+    ngOnInit() {
+        this.invoiceService
+            .getInvoices()
+            .snapshotChanges()
+            .subscribe(
+							update => {
+                this.invoiceList = this.getInvoice(update);
+            });
+    }
+
+    getInvoice(fireBaseData) {
+        const data = [];
+
+        fireBaseData.forEach(element => {
+            const inv = element.payload.toJSON();
+            inv['$key'] = element.key;
+            data.push(inv as Invoice);
+        });
+
+        return data;
+    }
 
 }
