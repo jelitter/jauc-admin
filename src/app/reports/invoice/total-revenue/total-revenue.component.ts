2f3e87d55e8008c0ad24b78af5dbb061098c5ac5
Dominic M Brause <dom.brause@gmail.com>
2019-01-07 23:05:27 +0000

    + total revenue done

--- src/app/reports/invoice/total-revenue/total-revenue.component.ts
+++ src/app/reports/invoice/total-revenue/total-revenue.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from 'src/app/services/invoice.service';
 import { Invoice } from 'src/app/models/invoice';
+import { Settlement } from 'src/app/models/settlement';
+import { Booking } from 'src/app/models/booking';
 
 @Component({
   selector: 'app-total-revenue',
@@ -10,6 +12,7 @@ import { Invoice } from 'src/app/models/invoice';
 export class TotalRevenueComponent implements OnInit {
 
     invoiceList: Array<Invoice>;
+		totalRevenue: number;
 
     constructor(private invoiceService: InvoiceService) {}
 
@@ -20,6 +23,7 @@ export class TotalRevenueComponent implements OnInit {
             .subscribe(
 							update => {
                 this.invoiceList = this.getInvoice(update);
+								this.totalRevenue = this.getRevenue(this.invoiceList);
             });
     }
 
@@ -34,5 +38,15 @@ export class TotalRevenueComponent implements OnInit {
 
         return data;
     }
+
+		getRevenue(invoices): number {
+			let total = 0;
+
+			invoices.forEach(element => {
+				total += element.price;
+			});
+
+			return total;
+		}
 
 }
